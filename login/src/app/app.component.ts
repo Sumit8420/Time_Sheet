caa7cb6c3b55748c78159bf20051d187a1486b6c
Sumit8420 <sumitkumarsingh495@gmail.com>
2022-06-30 12:37:26 +0530

    first commit login & register UI

---
+++ login/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demoApp';
+ email:string | undefined;
+ password:string | undefined;
+ remail:string | undefined;
+ rpassword:string | undefined;
+ rcpassword:string | undefined;
+
+  constructor(private snackBar:MatSnackBar){
+
+  }
+  register() {
+
+  }
+  login() {
+    if(this.email=="admin" && this.password=="admin"){
+        this.snackBar.open('Login Successful','',{duration:1000})
+    }else{
+      this.snackBar.open('Login error','',{duration:1000})
+    }
+  }
+}
